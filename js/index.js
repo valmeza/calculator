3c03e5b76484d512beb50426a36be70cbf6a6f6c
Valeria Meza <valeriameza173@gmail.com>
2020-06-05 21:02:36 -0500

    feat: added ability to add numbers when button is pressed.

--- js/index.js
+++ js/index.js
@@ -2,14 +2,19 @@
 
 const calculator = document.querySelector(".calculator");
 const keys = document.querySelector(".calculator-keys");
+const display = document.querySelector(".calculator-display");
 
 keys.addEventListener("click", (e) => {
   const key = e.target;
+  const keyContent = key.textContent;
   const action = key.dataset.action;
+  const displayNumber = display.textContent;
 
   if (!action) {
-    console.log("number key!");
-  } else {
-    console.log("action key!");
+    if (displayNumber === "0") {
+      display.textContent = keyContent;
+    } else {
+      display.textContent = displayNumber + keyContent;
+    }
   }
 });
