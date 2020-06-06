15da7d731c1adcef1b416f43a6f0c13e0a231c61
Valeria Meza <valeriameza173@gmail.com>
2020-06-05 21:54:21 -0500

    feat: when user hits on an operator and hit another number it replaces the display with the new num.

--- js/index.js
+++ js/index.js
@@ -9,12 +9,31 @@ keys.addEventListener("click", (e) => {
   const keyContent = key.textContent;
   const action = key.dataset.action;
   const displayNumber = display.textContent;
+  const previousKeyType = calculator.dataset.previousKeyType;
 
   if (!action) {
-    if (displayNumber === "0") {
+    if (displayNumber === "0" || previousKeyType === "operator") {
       display.textContent = keyContent;
     } else {
       display.textContent = displayNumber + keyContent;
     }
   }
+
+  if (action === "decimal") {
+    display.textContent = displayNumber + ".";
+  }
+
+  if (
+    action === "add" ||
+    action === "subtract" ||
+    action === "multiply" ||
+    action === "divide"
+  ) {
+    key.classList.add("is-depressed");
+    calculator.dataset.previousKeyType = "operator";
+  }
+
+  Array.from(key.parentNode.children).forEach((key) =>
+    key.classList.remove("is-depressed")
+  );
 });
